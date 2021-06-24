26356296bb19f2af05dd3b512328d2aa0beaa8d9
Ryan <arash.dsp2001@gmail>
2021-06-24 12:18:12 +0430

    done migration v1

---
+++ database/migrations/2021_06_24_072639_create_posters_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posters', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text("content");
+            $table->boolean('is_published') ->default(false);
+            $table->timestamps();
+            $table->unsignedBigInteger("user_id")->unsigned()->index();
+            $table->unsignedBigInteger("category_id")->unsigned()->index();
+            $table->foreign("category_id")->references("id")->on("categories");
+            $table->foreign("user_id")->references("id")->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posters');
+    }
+}
